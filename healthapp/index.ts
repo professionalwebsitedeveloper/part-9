0f1a593eb715612e0f7d084a30e74e722456aab6
tc849948 <student@developer.com>
2026-08-20 13:44:27 +0300

    fix(healthapp): use type-only imports for express Request/Response

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import { calculateBmi } from './bmiCalculator.ts';
 import { calculateExercises } from './exerciseCalculator.ts';
 
