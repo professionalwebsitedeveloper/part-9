7418917da371a3bcd4ace21bc9d679483d8ea77f
tc849948 <student@developer.com>
2026-08-20 15:30:26 +0300

    exercise11(patientor): add Diagnosis type and type diagnoses data

--- patientor/backend/data/diagnoses.ts
+++ patientor/backend/data/diagnoses.ts
@@ -1,4 +1,6 @@
-export const diagnoses = [
+import type { Diagnosis } from '../types.ts';
+
+export const diagnoses: Diagnosis[] = [
   { code: 'A1', name: 'Flu' },
   { code: 'B2', name: 'Cold' },
   { code: 'C3', name: 'Allergy' },
