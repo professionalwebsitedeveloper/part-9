713f1022edb4620acc3ff8be05f338492841772f
tc849948 <student@developer.com>
2026-08-21 10:18:04 +0300

    add Exercise 18

--- course/src/components/Content.tsx
+++ course/src/components/Content.tsx
@@ -1,3 +1,64 @@
+import type { CoursePart } from '../types'
+
+interface ContentProps {
+  parts: CoursePart[]
+}
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`)
+}
+
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case 'basic':
+      return (
+        <p>
+          {part.name} {part.exerciseCount}
+          <br />
+          <i>{part.description}</i>
+        </p>
+      )
+    case 'group':
+      return (
+        <p>
+          {part.name} {part.exerciseCount}
+          <br />group projects {part.groupProjectCount}
+        </p>
+      )
+    case 'background':
+      return (
+        <p>
+          {part.name} {part.exerciseCount}
+          <br />
+          <i>{part.description}</i>
+          <br />background material: {part.backgroundMaterial}
+        </p>
+      )
+    case 'special':
+      return (
+        <p>
+          {part.name} {part.exerciseCount}
+          <br />
+          <i>{part.description}</i>
+          <br />requirements: {part.requirements.join(', ')}
+        </p>
+      )
+    default:
+      return assertNever(part as never)
+  }
+}
+
+const Content = ({ parts }: ContentProps) => {
+  return (
+    <div>
+      {parts.map((p, i) => (
+        <Part key={i} part={p} />
+      ))}
+    </div>
+  )
+}
+
+export default Content
 type CoursePart = {
   name: string
   exerciseCount: number
