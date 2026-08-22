eb5a5faee59a75774e246041858da2963a14550d
tc849948 <student@developer.com>
2026-08-22 14:45:15 +0300

    add Exercise 33

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -10,6 +10,18 @@ type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
+
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
